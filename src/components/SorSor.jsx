1ff2a824d3b3018500ccf6ccd3b66861b87ae76e
Ramazan TÜFEKÇİ <4970844+ramazantufekci@users.noreply.github.com>
2026-08-11 09:51:58 +0300

    Update SorSor.jsx

--- src/components/SorSor.jsx
+++ src/components/SorSor.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import { useAuth } from '../context/AuthContext';
 function SorSor() {
   const navigate = useNavigate();
   const [question, setQuestion] = useState('');
