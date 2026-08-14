b4090a379e8dfd8d049547eaed2f6278f256b044
Ramazan TÜFEKÇİ <4970844+ramazantufekci@users.noreply.github.com>
2026-08-14 15:56:03 +0300

    Import index.css for Tailwind directives
    Ensure Tailwind directives are imported by adding index.css.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
 import { AuthProvider } from './context/AuthContext';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css' // 🌟 Tailwind direktiflerini barındıran dosyanın import edildiğinden emin olun
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
