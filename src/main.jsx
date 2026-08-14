ed9c87c42c3d929a3fc4928160e5c9ad8494bf9c
Ramazan TÜFEKÇİ <4970844+ramazantufekci@users.noreply.github.com>
2026-08-14 15:59:49 +0300

    Refactor imports in main.jsx
    Removed duplicate imports of React and ReactDOM.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot,ReactDOM  } from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
 import { AuthProvider } from './context/AuthContext';
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css' // 🌟 Tailwind direktiflerini barındıran dosyanın import edildiğinden emin olun
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
