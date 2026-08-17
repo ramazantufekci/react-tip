a662efd5ca19ff4e7b035d69082213b6b5d025f0
Ramazan TÜFEKÇİ <4970844+ramazantufekci@users.noreply.github.com>
2026-08-17 13:45:12 +0300

    Update AnketDetay.jsx

--- src/components/AnketDetay.jsx
+++ src/components/AnketDetay.jsx
@@ -20,6 +20,7 @@ const [loadingPoll, setLoadingPoll] = useState(true);
 
   useEffect(() => {
   const fetchPoll = async () => {
+    console.log(useParams);
     //if (!slug) return;
 
     setLoadingPoll(true);
